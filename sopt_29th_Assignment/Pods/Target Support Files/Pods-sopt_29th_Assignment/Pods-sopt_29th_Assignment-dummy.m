81dcd3abbef273aa712a9fc868fb4575bfdbad86
i-colours-u <i.colours.u@gmail.com>
2021-10-28 03:47:32 +0900

    [chore/#10] :moyai: pod setting
    ##### 제목은 최대 50 글자까지만 입력 ############## -> |
    
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    
    # --- COMMIT END ---
    # <타입> 리스트
    # feat       sparkles               : 기능(새로운 기능)
    # fix        beetle                 : 버그(버그 수정)
    # refactor   hammer                 : refacetor 리팩토링
    # style      art                    : 스타일( 코드 형식 변경, 로직 변경 없음)
    # doc        pencil                 : 문서(문서 추가, 수정, 삭제)
    # test       white_check_mark       : 테스트 코드 추가
    # chore      moyai                  : 기타 변경사항
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

---
+++ sopt_29th_Assignment/Pods/Target Support Files/Pods-sopt_29th_Assignment/Pods-sopt_29th_Assignment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sopt_29th_Assignment : NSObject
+@end
+@implementation PodsDummy_Pods_sopt_29th_Assignment
+@end
---
+++ sopt_29th_Assignment/Pods/Target Support Files/Pods-sopt_29th_Assignment/Pods-sopt_29th_Assignment-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_sopt_29th_AssignmentVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_sopt_29th_AssignmentVersionString[];
+
